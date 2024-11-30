1ad8fa17bf7f15a04d05b9872f82ebf08340196a
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-11-30 17:35:01 -0300

    :sparkles: feat: adicionando arquivos do laboratorio

---
+++ LABORATORIOS_MONITOR/l1.p3.c
@@ -0,0 +1,31 @@
+typedef struct {
+    int id;  //Código do produto
+    int quantidade;  //Quantitativo do produto em estoque (QMP)
+    float preco;  //Preço unitário do produto.
+} TProduto;
+
+typedef struct {
+    int qtdeProdutos;  //Quantidade máxima de diferentes produtos
+    TProduto *produtos; //Vetor contendo os detalhes de cada produto
+} TEstoque;
+
+void alocaEstoque(TEstoque *pEstoque){
+    scanf("%d", &pEstoque->qtdeProdutos);
+    pEstoque->produtos = (TProduto *) malloc(pEstoque->qtdeProdutos * sizeof(TProduto));
+}
+
+void listarProdutos(TEstoque *Estoque){
+    for (int i = 0; i < Estoque->qtdeProdutos; i++)
+    {
+        printf("ID: %d, Qt:%d, Preço: %.2f\n", Estoque->produtos[i].id, Estoque->produtos[i].quantidade, Estoque->produtos[i].preco);
+    }
+}
+
+int main() {
+    TEstoque Estoque;
+    alocaEstoque(&Estoque);
+    incluirProdutos(&Estoque); 
+    listarProdutos(&Estoque);
+    desalocaEstoque(&Estoque);
+    return 0;
+}
